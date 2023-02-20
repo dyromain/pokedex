56da16f03b9b20cfd965e1b4f27c5f5f669e4fc6
Romain Dyrac <romain.dyrac@gmail.com>
2023-02-20 15:54:47 +0100

    searchbar

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -11,30 +11,7 @@ const searchBar = () => {
 
  const pokemonresults = [
 
-  { name: "", continent: "Europe" },
-  { name: "India", continent: "Asia" },
-  { name: "Bolivia", continent: "South America" },
-  { name: "Ghana", continent: "Africa" },
-  { name: "Japan", continent: "Asia" },
-  { name: "Canada", continent: "North America" },
-  { name: "New Zealand", continent: "Australasia" },
-  { name: "Italy", continent: "Europe" },
-  { name: "South Africa", continent: "Africa" },
-  { name: "China", continent: "Asia" },
-  { name: "Paraguay", continent: "South America" },
-  { name: "Usa", continent: "North America" },
-  { name: "France", continent: "Europe" },
-  { name: "Botswana", continent: "Africa" },
-  { name: "Spain", continent: "Europe" },
-  { name: "Senegal", continent: "Africa" },
-  { name: "Brazil", continent: "South America" },
-  { name: "Denmark", continent: "Europe" },
-  { name: "Mexico", continent: "South America" },
-  { name: "Australia", continent: "Australasia" },
-  { name: "Tanzania", continent: "Africa" },
-  { name: "Bangladesh", continent: "Asia" },
-  { name: "Portugal", continent: "Europe" },
-  { name: "Pakistan", continent: "Asia" },
+  { name: "", move: "", ability:"" },
 
 ];
 
@@ -60,7 +37,8 @@ return <div>
 <table>
   <tr>
     <th>Country</th>
-    <th>Continent</th>
+    <th>Move</th>
+    <th>Ability</th>
   </tr>
 
 {pokemonresults.map((pokemonresult, *index*) => {
@@ -68,7 +46,8 @@ return <div>
 <div>
   <tr>
     <td>{pokemonresult.name}</td>
-    <td>{pokemonresult.continent}</td>
+    <td>{pokemonresult.move}</td>
+    <td>{pokemonresult.ability}</td>
   </tr>
 </div>
 
